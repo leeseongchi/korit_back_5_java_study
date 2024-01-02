3f780cedea61aff765277a134e9d473c3df8e375
leeseongchi <kyoti0414@naver.com>
2024-01-02 17:44:02 +0900

    변경사항 추가

--- src/main/java/com/study/ch04/Operation07.java
+++ src/main/java/com/study/ch04/Operation07.java
@@ -4,5 +4,6 @@ public class Operation07 {
     public static void main(String[] args) {
         System.out.println("Git");
         System.out.println("Github");
+        System.out.println("변경사항 추가");
     }
 }
